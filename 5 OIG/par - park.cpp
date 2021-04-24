c0594ed42a6785231a951ded2a3b4ce9fcf16aae
Igor Jozefowicz <igorjozefowicz@gmail.com>
2021-04-24 23:03:10 +0200

    add par (park) 5oig proper solution
    - change name of previous to brute

--- 5 OIG/par - park.cpp
+++ 5 OIG/par - park.cpp
@@ -1,34 +1,45 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
+	int n;
+	int maxRight = 0;
+	int maxLeft = 0;
 
-    int length;
-    cin >> length;
-    int* heights = new int[length];
+	cin >> n;
+	vector<int> nums(n);
+	vector<int> maxLeftNums(n);
+	vector<int> maxRightNums(n);
 
-    for (int i = 0; i < length; i++) {
-        cin >> heights[i];
-    }
+	for (int i = 0; i < n; i++) {
+		cin >> nums[i];
+	}
 
-    int maxLeft = heights[0];
-    int maxRight = *max_element(heights, heights + length);
+	for (int i = 0; i < n; i++) {
+		if (maxLeft < nums[i]) {
+			maxLeft = nums[i];
+		}
 
-    for (int i = 0; i < length; i++) {
-        if (maxLeft < heights[i]) {
-            maxLeft = heights[i];
-        }
-        if (maxRight == heights[i] || maxRight == heights[i - 1]) {
-            maxRight = *max_element(heights + i, heights + length);
-        }
+		maxLeftNums[i] = maxLeft;
+	}
 
-        cout << maxLeft << " " << maxRight << endl;
-    }
+	for (int i = n - 1; i >= 0; i--) {
+		if (maxRight < nums[i]) {
+			maxRight = nums[i];
+		}
+
+		maxRightNums[i] = maxRight;
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << maxLeftNums[i] << " " << maxRightNums[i] << "\n";
+	}
 }
